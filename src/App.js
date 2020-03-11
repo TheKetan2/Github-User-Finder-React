18b6babbad9311a054d12f606c68e2b32f742990
Ketan <ketantoo@gmail.com>
2020-03-11 13:58:24 +0530

    used Axios and fetched user data from github api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/user/Users";
 
 class App extends React.Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  componentDidMount = async () => {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+
+    this.setState({
+      users: res.data,
+      loading: false
+    });
+  };
+
   render() {
     return (
       <div>
         <Navbar title={"Github Profile Finder"} />
         <div className="container">
-          <Users />
+          <Users users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     );
--- src/components/user/Users.js
+++ src/components/user/Users.js
@@ -2,35 +2,11 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "1",
-        login: "TheKetan2",
-        avatar_url:
-          "https://avatars2.githubusercontent.com/u/8502835?s=460&v=4",
-        html_url: "https://www.github.com/theketan2"
-      },
-      {
-        id: "2",
-        login: "TheKetan2",
-        avatar_url:
-          "https://avatars2.githubusercontent.com/u/8502835?s=460&v=4",
-        html_url: "https://www.github.com/theketan2"
-      },
-      {
-        id: "3",
-        login: "TheKetan2",
-        avatar_url:
-          "https://avatars2.githubusercontent.com/u/8502835?s=460&v=4",
-        html_url: "https://www.github.com/theketan2"
-      }
-    ]
-  };
   render() {
+    console.log("Users:", this.props.users);
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
